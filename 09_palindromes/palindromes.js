91dd29c4452ebc2014f317351cf2fe75dbfecd57
Prakash Stirret <102489686+pstirret@users.noreply.github.com>
2025-03-25 21:29:17 -0700

    Add my solution for Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (inStr) {
+   
+   //Convert input string to an array
+   inStrArr = inStr.split("");
+
+   //Filter out all the non-letters using a regex
+   lettersOnly = inStrArr.filter(letter => /^[a-zA-Z0-9]+$/.test(letter));
+
+   //Convert to lower case
+   lettersOnly = lettersOnly.map(letter => letter.toLowerCase());
+
+   for (let i = 0; i < lettersOnly.length; i++) {
+      if (i >= (lettersOnly.length - 1 - i)) {
+         return true;
+      }
+      if (lettersOnly[i] != lettersOnly[lettersOnly.length - 1 - i]) {
+         return false;
+      }
+   }
 
 };
 
